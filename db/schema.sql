cb9fbeb17bdaeb8945df9ec57e4a1698ac8fb070
Amanda Lang <Cadenc2003@icloud.com>
2024-10-08 14:38:09 -0500

    set up file structure, set up schema

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+\c employee_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    department_name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY department_id
+    REFERENCES department(id)
+);
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    FOREIGN KEY role_id
+    REFERENCES role(id)
+);
